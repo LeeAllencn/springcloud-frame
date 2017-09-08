55df7cc078c0a348ed3e9b97ee00956277f7c904
lixin <lixin@hnair.com>
2017-09-08 11:20:33 +0800

    add hystrix for service consumer

--- springcloud-frame-consumer/src/main/java/com/rocky/cloud/feign/HelloRemote.java
+++ springcloud-frame-consumer/src/main/java/com/rocky/cloud/feign/HelloRemote.java
@@ -1,5 +1,6 @@
 package com.rocky.cloud.feign;
 
+import com.rocky.cloud.feign.hystrix.HelloRemoteHystrix;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 /**
  * Created by Rocky on 2017-09-08.
  */
-@FeignClient(name= "microservice-provider")
+@FeignClient(name= "microservice-provider", fallback = HelloRemoteHystrix.class)
 public interface HelloRemote {
     @RequestMapping(value = "/hello")
     public String hello(@RequestParam(value = "name") String name);
---
+++ springcloud-frame-consumer/src/main/java/com/rocky/cloud/feign/hystrix/HelloRemoteHystrix.java
@@ -0,0 +1,17 @@
+package com.rocky.cloud.feign.hystrix;
+
+import com.rocky.cloud.feign.HelloRemote;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Rocky on 2017-09-08.
+ */
+@Component
+public class HelloRemoteHystrix implements HelloRemote {
+
+    @Override
+    public String hello(@RequestParam(value = "name") String name) {
+        return "hello " +name+", this messge send failed ";
+    }
+}
