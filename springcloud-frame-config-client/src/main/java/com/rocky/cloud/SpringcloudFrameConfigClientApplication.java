fc4ab95057e12c9b4401d79eb16a2e0e3d17c4a6
lixin <lixin@hnair.com>
2017-09-08 16:16:40 +0800

    service config client

--- springcloud-frame-config-client/src/main/java/com/rocky/cloud/SpringcloudFrameConfigClientApplication.java
+++ springcloud-frame-config-client/src/main/java/com/rocky/cloud/SpringcloudFrameConfigClientApplication.java
@@ -2,8 +2,10 @@ package com.rocky.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringcloudFrameConfigClientApplication {
 
 	public static void main(String[] args) {
