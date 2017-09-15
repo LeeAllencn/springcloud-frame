6491f902a2234801c1b41d628ee94007e8585f59
lixin <lixin@hnair.com>
2017-09-15 10:25:29 +0800

    zuul service

--- springcloud-frame-zuul/src/main/java/com/rocky/cloud/SpringcloudFrameZuulApplication.java
+++ springcloud-frame-zuul/src/main/java/com/rocky/cloud/SpringcloudFrameZuulApplication.java
@@ -2,8 +2,10 @@ package com.rocky.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class SpringcloudFrameZuulApplication {
 
 	public static void main(String[] args) {
