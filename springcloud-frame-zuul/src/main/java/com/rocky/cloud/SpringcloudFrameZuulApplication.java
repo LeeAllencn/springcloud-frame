411f801da7665650ff5d2e5e85f5ae7d649246ca
lixin <lixin@hnair.com>
2017-09-22 10:29:23 +0800

    zuul filter

--- springcloud-frame-zuul/src/main/java/com/rocky/cloud/SpringcloudFrameZuulApplication.java
+++ springcloud-frame-zuul/src/main/java/com/rocky/cloud/SpringcloudFrameZuulApplication.java
@@ -1,8 +1,10 @@
 package com.rocky.cloud;
 
+import com.rocky.cloud.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -11,4 +13,9 @@ public class SpringcloudFrameZuulApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudFrameZuulApplication.class, args);
 	}
+
+	/*@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
+	}*/
 }
---
+++ springcloud-frame-zuul/test.md
@@ -0,0 +1,5 @@
+# 路由测试
+```
+http://localhost:8888/provider/hello?name=lee
+http://localhost:8888/consumer/hello/li
+```
